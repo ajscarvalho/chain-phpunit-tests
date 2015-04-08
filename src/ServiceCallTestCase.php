85effab369ed9f9ef5a7f43558fdbe634d8d82f8
Alexandre José Amaral de Carvalho <alexandre-a-carvalho@telecom.pt>
2015-04-08 15:52:58 +0100

    working test abstraction for services

--- src/ServiceCallTestCase.php
+++ src/ServiceCallTestCase.php
@@ -10,34 +10,38 @@ class ServiceCallTestCase extends \PHPUnit_Framework_TestCase {
     private $serviceHandler;
     private $serviceResult;
 
-    public function __construct($result) { $this->serviceResult = $result; }
+    public function __construct($result = null) { $this->serviceResult = $result; }
 
-    public function setUp($serviceHandler) { $this->serviceHandler = $serviceHandler; }
+    protected function setUp($serviceHandler) { $this->serviceHandler = $serviceHandler; }
 
     public function call($function, $params)
     {
         try {
             $this->serviceResult = call_user_func_array(array($this->serviceHandler, $function), $params);
-            return $this;
+
         } catch(RequestException $e) { 
             $this->fail('Call to operation ' . get_class($this->serviceHandler) . '::' . $function . 
                 " failed with exception: \n". GuzzleWrapper::parseExceptionMessage($e)); 
         }
+        return $this;
     }
 
     public function expectsObject($objectType, $propertyValidation)
     {
-        $this->assertInstanceOf($objectType, $this->serviceResult, "Must return instance of $object");
+        $this->assertInstanceOf($objectType, $this->serviceResult, "Must return instance of $objectType");
         foreach($propertyValidation as $property => $expectedValue)
         {
+            $this->assertObjectHasAttribute($property, $this->serviceResult, "Expected $objectType to have attribute $property");
             $actualValue = $this->serviceResult->$property;
             $this->assertEquals($actualValue, $expectedValue, "Expected property $property to have $expectedValue, instead found $actualValue");
         }
+        return $this;
     }
 
     public function expectsNativeType($nativeType)
     {
         $this->assertInternalType($nativeType, $this->serviceResult, "Must return instance of $nativeType");
+        return $this;
     }
 
     public function sub($property, $arrayIndex = null)
